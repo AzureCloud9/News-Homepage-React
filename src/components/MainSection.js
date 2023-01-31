9e9b79b063ba557523da04a7852887aa7abe43a6
Azure <marleyalmere@gmail.com>
2023-01-31 23:55:59 +0100

    Uodate 1

---
+++ src/components/MainSection.js
@@ -0,0 +1,10 @@
+import mainImage from './assets/images/image-web-3-mobile.jpg'
+
+export default function MainSection () {
+    return (
+        <div className="mainSection">
+            <img className="mainImage" src={mainImage} alt=""/>
+            
+        </div>
+    )
+}
---
+++ src/components/MenuLogo.js
@@ -0,0 +1,37 @@
+import logo from './assets/images/logo.svg'
+import closeMenu from './assets/images/icon-menu-close.svg'
+import menuMobile from './assets/images/icon-menu.svg'
+import React, { useState } from 'react';
+
+
+
+
+
+export default function MenuLogo () {
+const [exit, setExit] = useState(false)
+
+function handleClick () {
+    setExit(!exit)
+}
+
+
+    return (
+        <div className="module1">
+            <img className='img1' src={logo} alt=""/>
+            <img onClick={handleClick} className='menu' src={menuMobile} alt=''/>
+            {exit &&
+            <div className="menuBackground" onClick={handleClick}>
+                <div className='menuList'  >
+                    <img onClick={handleClick} className='menuClose' src={closeMenu} alt=''/>
+                    <ul>
+                        <li><a href='test'>Home</a></li>
+                        <li><a href='test'>New</a></li>
+                        <li><a href='test'>Popular</a></li>
+                        <li><a href='test'>Trending</a></li>
+                        <li><a href='test'>Categories</a></li>
+                    </ul>
+                </div>
+            </div>}
+        </div>
+    )
+}
